c944d1917d40157eb77a718841b3edf7c2132ca8
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-09 17:10:23 -0500

    nav was a div....

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -1,11 +1,11 @@
 function Nav() {
   return (
-    <div>
+    <nav>
       <a href='/About'>About</a>
       <a href='/Portfolio'>Portfolio</a>
       <a href='/Resume'>Resume</a>
       <a href='/Contact'>Contact</a>
-    </div>
+    </nav>
   );
 }
 
