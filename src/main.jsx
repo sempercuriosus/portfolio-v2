bc7392a995194faf3d95edd176865fadfcf665cd
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-09 17:03:47 -0500

    added additional pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./style/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './style/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Pages
+import App from './App.jsx';
+import Error from './components/Error/error.jsx';
+import About from './components/About/about.jsx';
+import Contact from './components/Contact/contact.jsx';
+import Portfolio from './components/Portfolio/portfolio.jsx';
+import Project from './components/Project/project.jsx';
+import Resume from './components/Resume/resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        // index: true,
+        path: '/',
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'project',
+        element: <Project />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
