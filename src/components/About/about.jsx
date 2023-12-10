856ef65f27d91903eb986e2d0a78c83109319b38
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-10 13:09:03 -0500

    moved tech experience from about

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -24,34 +24,6 @@ function About() {
         behind things.
       </div>
 
-      <div id='dev_experience'>
-        <ul>
-          <h3>Tech Stack</h3>
-          <li>Javascript</li>
-          <li>Express</li>
-          <li>Node</li>
-          <li>Python</li>
-          <li>C#</li>
-          <li>React</li>
-          <li>T-Sql and NoSql</li>
-          <li>MongoDB</li>
-        </ul>
-
-        <h3>Other Projects</h3>
-        <li>
-          Recently, I have been learning some about Rasperry Pi. Here are my
-          projects:
-        </li>
-        <ul>
-          <li>Pi Hole</li>
-          <li>Retro Pi</li>
-          <li>
-            I have setup a dedicated Pi for a Homelab server to deploy projects
-            to
-          </li>
-        </ul>
-      </div>
-
       <div id='dev_hobby'>
         <strong>Outside of work I like to:</strong>
         <ul>
--- src/components/Resume/resume.jsx
+++ src/components/Resume/resume.jsx
@@ -2,7 +2,38 @@ function Resume() {
   document.title = 'Resume | Third Coast 🦡';
   return (
     <section id='resume'>
-      <p>here it is</p>
+      <div id='dev_experience'>
+        <ul>
+          <h3>Tech Stack</h3>
+          <li>Javascript</li>
+          <li>Express</li>
+          <li>Node</li>
+          <li>Python</li>
+          <li>C#</li>
+          <li>React</li>
+          <li>T-Sql and NoSql</li>
+          <li>MongoDB</li>
+        </ul>
+
+        <h3>Other Projects</h3>
+        <li>
+          Recently, I have been learning some about Rasperry Pi. Here are my
+          projects:
+        </li>
+        <ul>
+          <li>Pi Hole</li>
+          <li>Retro Pi</li>
+          <li>
+            I have setup a dedicated Pi for a Homelab server to deploy projects
+            to
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>
+          My resume is downloadable and can be found <a href='#'>here</a>.
+        </p>
+      </div>
     </section>
   );
 }
