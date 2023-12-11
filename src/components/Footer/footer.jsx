0ba35fb7e75c27cb7a5188e73c6d65e9d59dab9c
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-10 20:00:59 -0500

    added footer style

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 const links = {
   github: 'https://github.com/sempercuriosus',
   email: 'mailto:' + 'hulse@hey.com',
@@ -6,22 +8,24 @@ const links = {
 
 function Footer() {
   return (
-    <footer>
-      <p>
-        I do not use social media, but you can{' '}
-        <a href={links.email}>Email Me</a>. It may be a day or two for me to
-        return.
-      </p>
-      <p>
-        Checkout my GitHub <a href={links.github}>landing page</a> and see what
-        I am up to.
-      </p>
-      <p>
-        I occasionally post to my <a href={links.blog}>blog</a>, so please check
-        out the posts, if you like. This is very new, but I would still like to
-        share.
-      </p>
-    </footer>
+    <Fragment>
+      <br />
+      <br />
+      <footer className='footer'>
+        <div className='container'>
+          <p>
+            Checkout my GitHub <a href={links.github}>landing page</a> and see
+            what I am up to.
+          </p>
+
+          <p>
+            I occasionally post to my <a href={links.blog}>blog</a>, so please
+            check out the posts, if you like. This is very new, but I would
+            still like to share.
+          </p>
+        </div>
+      </footer>
+    </Fragment>
   );
 }
 
