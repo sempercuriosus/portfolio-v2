c51e2925bc0c7c31d4af3c2ec57175b20a304051
sempercuriosus <vespers-garages.0n@icloud.com>
2023-12-11 10:38:04 -0500

    added block

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -11,12 +11,12 @@ function Footer() {
     <Fragment>
       <footer className='footer'>
         <div className='container'>
-          <p>
+          <p className='block'>
             Checkout my GitHub <a href={links.github}>landing page</a> and see
             what I am up to.
           </p>
 
-          <p>
+          <p className='block'>
             I occasionally post to my <a href={links.blog}>blog</a>, so please
             check out the posts, if you like. This is very new, but I would
             still like to share.
