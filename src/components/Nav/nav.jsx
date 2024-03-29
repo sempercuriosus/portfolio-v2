297aef9b3b390a53f04e378395232caf6ee534de
Eric <vespers-garages.0n@icloud.com>
2024-03-28 21:44:36 -0400

    updated nav with burger menu

--- src/components/Nav/nav.jsx
+++ src/components/Nav/nav.jsx
@@ -1,54 +1,82 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 // eslint-disable-next-line react/prop-types
 function Nav({ developerName }) {
+  const [isActive, setisActive] = useState(false);
+
   return (
-    // Main Nav Group
-    <nav
-      id='nav-nav'
-      className='navbar is-spaced has-background-dark'
-      role='navigation'>
-      {/* About is the root route, href reflects this */}
-      <div
-        id='dev-name'
-        className='navbar-item has-background-dark'>
-        {developerName} <em id='name-div'>|</em>
-      </div>
+    <>
+      <nav
+        id='nav-nav'
+        className='navbar is-spaced has-background-dark'
+        role='navigation'
+        aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <div
+            id='dev-name'
+            className='navbar-item has-background-dark'>
+            {developerName} <em id='name-div'>|</em>
+          </div>
 
-      {/* Links */}
+          <a
+            role='button'
+            className={`navbar-burger burger has-text-light ${
+              isActive ? 'is-active' : ''
+            }`}
+            aria-label='menu'
+            aria-expanded='false'
+            onClick={() => {
+              setisActive(!isActive);
+            }}>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+            <span aria-hidden='true'></span>
+          </a>
+        </div>
 
-      {/* About */}
-      <Link
-        id='link-about'
-        to='/'
-        className='navbar-item has-text-light'>
-        <strong>ABOUT</strong>
-      </Link>
+        {/* Links */}
+        <div
+          id='navbar-menu'
+          className={`navbar-menu has-background-dark ${
+            isActive ? 'is-active' : ''
+          }`}>
+          <div className='navbar-start'>
+            {/* About */}
+            <Link
+              id='link-about'
+              to='/'
+              className='navbar-item has-text-light'>
+              <strong>ABOUT</strong>
+            </Link>
 
-      {/* Portfolio */}
-      <Link
-        id='link-portfolio'
-        to='/Portfolio'
-        className='navbar-item has-text-light'>
-        <strong>PORTFOLIO</strong>
-      </Link>
+            {/* Portfolio */}
+            <Link
+              id='link-portfolio'
+              to='/Portfolio'
+              className='navbar-item has-text-light'>
+              <strong>PORTFOLIO</strong>
+            </Link>
 
-      {/* Resume */}
-      <Link
-        id='link-resume'
-        to='/Resume'
-        className='navbar-item has-text-light'>
-        <strong>RESUME</strong>
-      </Link>
+            {/* Resume */}
+            <Link
+              id='link-resume'
+              to='/Resume'
+              className='navbar-item has-text-light'>
+              <strong>RESUME</strong>
+            </Link>
 
-      {/* Contact */}
-      <Link
-        id='link-contact'
-        to='/Contact'
-        className='navbar-item has-text-light'>
-        <strong>CONTACT</strong>
-      </Link>
-    </nav>
+            {/* Contact */}
+            <Link
+              id='link-contact'
+              to='/Contact'
+              className='navbar-item has-text-light'>
+              <strong>CONTACT</strong>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 
