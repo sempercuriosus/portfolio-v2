3e672b6e950dbc60b34787dbb8a0350752a7ffc9
Eric <vespers-garages.0n@icloud.com>
2024-03-29 18:51:19 -0400

    added contact button component and re-rendering the contact page

--- src/components/ui/contactButton.jsx
+++ src/components/ui/contactButton.jsx
@@ -1,28 +1,46 @@
 import PropTypes from 'prop-types';
 
 function ContactButton(props) {
+  //
+
+  const openLink = (action, target) => {
+    if (action == 'email') {
+      window.location.href = 'mailto:' + target;
+    } else if (action == 'github') {
+      window.open(target, '_blank', 'noopener,noreferrer');
+    } else {
+      console.log('Action not found');
+    }
+  };
+
   return (
-    // icon, name, color
-    <div className='column is-narrow is-offset-1'>
-      <button
-        className={'button is-large ' + props.text + ' ' + props.color}
-        style={{ width: '250px', padding: '3rem 0' }}
-        onClick={props.action}>
-        <span className='icon is-medium'>
-          <i>{props.icon}</i>
-        </span>
-        <span className='is-size-4 has-text-weight-semibold'>{props.name}</span>
-      </button>
-    </div>
+    <button
+      className={'button is-large ' + props.text + ' ' + props.color}
+      style={{
+        width: '100%',
+        maxWidth: '350px',
+        height: '100%',
+        maxHeight: '250px',
+        padding: '3rem 1rem',
+      }}
+      onClick={() => {
+        openLink(props.action, props.target);
+      }}>
+      <span className='icon is-medium'>
+        <i>{props.icon}</i>
+      </span>
+      <span className='is-size-4 has-text-weight-semibold'>{props.name}</span>
+    </button>
   );
 }
 
 ContactButton.propTypes = {
   name: PropTypes.string,
   text: PropTypes.string,
+  icon: PropTypes.string,
   color: PropTypes.string,
   action: PropTypes.string,
-  icon: PropTypes.string,
+  target: PropTypes.string,
 };
 
 export default ContactButton;
