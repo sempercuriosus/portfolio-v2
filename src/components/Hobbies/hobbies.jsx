4d579fa1bb1db5ac47fb705efd53383ff4b155d1
Eric <vespers-garages.0n@icloud.com>
2024-03-28 20:53:32 -0400

    init on hobbies

---
+++ src/components/Hobbies/hobbies.jsx
@@ -0,0 +1,28 @@
+function Hobbies() {
+  return (
+    <>
+      <div
+        id='dev-hobby'
+        className='content'>
+        <div
+          id='hobby-title'
+          className='title is-4'>
+          Other Interests
+        </div>
+
+        {/* Hobbies List*/}
+        <div id='hobby-list'>
+          <p>Hiking</p>
+          <p>Disc Golfing</p>
+          <p>Woodworking and Building</p>
+          <p>Exploring new-to-me places and revisiting previous places</p>
+          <p>Reading</p>
+          <p>Most importantly, spending time with my family</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Hobbies;
+
