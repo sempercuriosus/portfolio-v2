18a401162813f3d7cddf34067cf12cf62283dccf
Eric <vespers-garages.0n@icloud.com>
2024-03-29 19:39:00 -0400

    Condencing what I have from github to the about page here.

--- src/components/Hobbies/hobbies.jsx
+++ src/components/Hobbies/hobbies.jsx
@@ -7,7 +7,17 @@ function Hobbies() {
         <div
           id='hobby-title'
           className='title is-4'>
-          Other Interests
+          Some Other Interests
+        </div>
+
+        <div className='block'>
+          <p>
+            Outside of the technical realm, I am drawn to nature, and I find
+            solace in the outdoors. It helps me appreciate things. When I take
+            moments to watch the clouds drifting by, listen to the symphony of
+            sounds put on by all the inhabitants, or to spend a few minutes
+            outside to watch the sunrise. I love it.
+          </p>
         </div>
 
         {/* Hobbies List*/}
