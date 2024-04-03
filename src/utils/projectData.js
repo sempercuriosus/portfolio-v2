7e12bfc00d76ef3f3ef00017f6046c0e1eab076a
Eric <vespers-garages.0n@icloud.com>
2024-04-03 19:42:35 -0400

    pulled out.

---
+++ src/utils/projectData.js
@@ -0,0 +1,69 @@
+// Sample Project
+// {
+// featuredProject: false,
+// projectCode: 'readme',
+//   title: 'readme helper',
+//   description: '',
+//   codeRepo: '',
+//   deployedLink: '',
+// },
+
+const projects = [
+  {
+    featuredProject: true,
+    projectCode: 'dyfab',
+    title: "don't you forget about me",
+    description:
+      'While planning a family day, my mother-in-law "forgot I was a person", so I decided to remind her that I was by sending her an email of me every day!',
+    codeRepo: 'https://github.com/sempercuriosus/dont_you_forget_about_me',
+    deployedLink: '',
+  },
+  {
+    featuredProject: false,
+    projectCode: 'shws',
+    title: 'study hacks blog web scrape',
+    description:
+      'I wanted to learn more about Web Scraping, Python, and what Cal Newport has to offer. I hit a trifecta here with this project.',
+    codeRepo: 'https://github.com/sempercuriosus/study_hacks_scrape_public',
+    deployedLink: '',
+  },
+  {
+    featuredProject: false,
+    projectCode: 'doom',
+    title: 'dooms date algorithm',
+    description:
+      "Taking inspiration from John Conway's Doomsday Algorithm, I made this little script to return the day of the week any date falls on.",
+    codeRepo: 'https://github.com/sempercuriosus/doomsdate_public',
+    deployedLink: '',
+  },
+  {
+    featuredProject: false,
+    projectCode: 'fittr',
+    title: 'fitness tracker',
+    description:
+      'This was our first group project, and I was in a Developer role. I am  responsible for the HTML after the initial framework, CSS for layout to make the application mobile friendly, and Javascript General Functionality and Exercise API integration.',
+    codeRepo: 'https://github.com/vonjareew/FitnessTracker',
+    deployedLink: 'https://vonjareew.github.io/FitnessTracker/',
+  },
+  {
+    featuredProject: false,
+    projectCode: 'tool',
+    title: 'tool rental application',
+    description:
+      'This was our second project, and I was in a Project Manager role as well as Developer. Making me responsible for the API, Front-End funtionality, and Back-End functionality.',
+    codeRepo: 'https://github.com/sempercuriosus/tool-manager',
+    deployedLink:
+      'https://rocky-journey-83321-4f006da2de0c.herokuapp.com/login',
+  },
+  {
+    featuredProject: true,
+    projectCode: 'statelinefc',
+    title: 'Stateline Family Chiropractic',
+    description:
+      'I put this website together for a local Chiropractor, my wife, when she wanted to refresh what her website looked like.',
+    codeRepo: 'https://github.com/sempercuriosus/stateline-fc',
+    deployedLink: 'https://statelinechiro.com',
+  },
+];
+
+export default projects;
