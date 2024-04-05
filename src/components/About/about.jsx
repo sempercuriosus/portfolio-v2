a71b84b4777e9cbc230b993ac20410b1d912da4e
Eric <vespers-garages.0n@icloud.com>
2024-04-04 21:41:23 -0400

    updated pic

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Hobbies from '../Hobbies/hobbies';
 import SectionTitle from '../ui/sectionTitle';
 
-const dev_picPath = '/images/eric-dev-pic-2.JPG';
+const dev_picPath = '/images/eric-dev-pic-2.jpg';
 
 function About() {
   document.title = 'About | Third Coast 🦡';
@@ -14,8 +14,10 @@ function About() {
 
   return (
     <>
-      <section id='about'>
-        <div className='section is-large title is-1 block has-text-centered'>
+      <section
+        id='about'
+        className='is-size-5'>
+        <div className='section is-large title is-2 block has-text-centered'>
           Welcome To My <span className='orangeify'>Portfolio</span>
           <span
             id='blinky'
@@ -26,92 +28,92 @@ function About() {
         <hr />
         <br />
 
-        <div className='is-size-5'>
-          <div className='columns is-multiline is-vcentered'>
-            <div className='column is-narrow'>
-              <figure
-                id='dev-photo'
-                className=''>
-                <img
-                  src={dev_picPath}
-                  alt='Developer Profile Image'
-                  className='image has-ratio'
-                  width='300'
-                  style={{ borderRadius: '6px' }}
-                />
-              </figure>
-            </div>
+        <div className='block'>
+          {/* <div
+            className='columns is-multiline is-centered'
+            style={{ flexDirection: 'row-reverse', display: 'flex' }}> */}
+          {/* <div className='column is-3'>
+              
+            </div> */}
 
-            <div className='column is-offset-1'>
-              <SectionTitle
-                id='about-title'
-                title='Hello! 👋'
-              />
+          {/* <div className='column is-9 block'> */}
 
-              <div className='block'>
-                <p>
-                  My name is{' '}
-                  <span
-                    id='dev_name'
-                    className='has-text-weight-bold'>
-                    Eric
-                  </span>{' '}
-                  and I am a Full Stack Web Developer currently based in South
-                  Carolina, USA, originally hailing from Wisconsin 🧀.
-                </p>
-              </div>
+          <SectionTitle
+            id='about-title'
+            title='Hello! 👋'
+          />
 
-              <div className='block'>
-                <p>I love working with my hands, crafting, and creating.</p>
-              </div>
-
-              <div className='block'>
-                <p>
-                  I have a genuine joy for learning, which is why I chose the
-                  username <strong>Semper Curiosus</strong>. It is my curiosity
-                  about what surounds me drives me to understand the{' '}
-                  <em>why</em> behind them.
-                  {/* I am always eager to explore the
-                  world around me. */}
-                </p>
-              </div>
-
-              <div className='block'>
-                <p>
-                  I embrace the process of trying, to get started and make
-                  mistakes, recognizing that perfection is not the goal. I
-                  believe that technology that empowers people, provides
-                  solutions, remains unintrusive, and simplifies life. I aim to
-                  make that kind of technology.
-                </p>
-              </div>
-              {/* Hobbies*/}
-              <Hobbies />
-            </div>
+          <div className='block'>
+            <p>
+              My name is{' '}
+              <span
+                id='dev_name'
+                className='has-text-weight-bold'>
+                Eric
+              </span>{' '}
+              and I am a Full Stack Web Developer currently based in South
+              Carolina, USA, originally hailing from Wisconsin 🧀.
+            </p>
           </div>
 
-          <br />
-
-          <div className='section has-text-centered'>
-            Here are some of my{' '}
-            <strong>
-              <Link
-                id='link-portfolio-about'
-                to='/Portfolio'>
-                selected works
-              </Link>
-            </strong>{' '}
-            or if you would like to{' '}
-            <strong>
-              <Link
-                id='link-contact-about'
-                to='/Contact'>
-                connect
-              </Link>
-            </strong>{' '}
-            with me
+          <div className='block'>
+            <p>I love working with my hands, crafting, and creating.</p>
           </div>
         </div>
+
+        <br />
+
+        <figure className='block image'>
+          <img
+            src={dev_picPath}
+            alt='Developer Profile Image'
+            className='container'
+            style={{ borderRadius: '10px', width: '80%' }}
+          />
+        </figure>
+
+        <div className='block'>
+          <p>
+            I have a genuine joy for learning, which is why I chose the username{' '}
+            <strong>Semper Curiosus</strong>. It is my curiosity about what
+            surounds me drives me to understand the <em>why</em> behind them.
+            {/* I am always eager to explore the
+                  world around me. */}
+          </p>
+
+          <p>
+            I embrace the process of trying, to get started and make mistakes,
+            recognizing that perfection is not the goal. I believe that
+            technology that empowers people, provides solutions, remains
+            unintrusive, and simplifies life. I aim to make that kind of
+            technology.
+          </p>
+        </div>
+
+        {/* Hobbies*/}
+        <Hobbies />
+        <br />
+
+        <div className='section has-text-centered'>
+          Here are some of my{' '}
+          <strong>
+            <Link
+              id='link-portfolio-about'
+              to='/Portfolio'>
+              selected works
+            </Link>
+          </strong>{' '}
+          or if you would like to{' '}
+          <strong>
+            <Link
+              id='link-contact-about'
+              to='/Contact'>
+              connect
+            </Link>
+          </strong>{' '}
+          with me
+        </div>
+        {/* </div> */}
       </section>
     </>
   );
