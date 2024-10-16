6cce30d988ef6bcc6dc7db1deee8affd781d8430
Eric <vespers-garages.0n@icloud.com>
2024-10-16 10:46:24 -0400

    updated about me

--- src/components/Hobbies/hobbies.jsx
+++ src/components/Hobbies/hobbies.jsx
@@ -3,38 +3,22 @@ function Hobbies() {
     <>
       <div
         id='dev-hobby'
-        className='content'>
-        <div
-          id='hobby-title'
-          className='title is-4'>
-          Some Other Interests
-        </div>
-
-        <div className='block'>
-          <p>
-            Outside of the technical realm, I am drawn to nature
-            and exploration, and I find solace in the outdoors.
-            It helps me appreciate things. I love it. I take
-            moments to watch the clouds drifting by, listen to
-            the sounds around me, or to spend a few minutes
-            outside to watch the sunrise.
-          </p>
-
-          <br />
-        </div>
+        className='content block'>
+        <div className='block'></div>
 
         {/* Hobbies List*/}
         <div id='hobby-list'>
-          <p className='title is-5'>
-            Here are some of my hobbies
-          </p>
+          <p className='title is-5'>Interests</p>
 
-          <p>Hiking</p>
-          <p>Disc Golfing</p>
-          <p>Woodworking and Building</p>
-          <p>Exploring new and revisiting previous places</p>
-          <p>Reading</p>
-          <p>Most importantly, spending time with my family</p>
+          <p>
+            I love getting out and going for a long{' '}
+            <strong>Hike</strong>, playing a round or two of{' '}
+            <strong>Disc Golf</strong> on anywhere, designing and
+            building in my <strong>Woodworking</strong> shop,
+            getting out to <strong>Explore</strong> new and
+            seeing places I have been before, and I love to{' '}
+            <strong>Read</strong>.
+          </p>
         </div>
       </div>
     </>
